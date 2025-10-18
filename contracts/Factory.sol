91ec907f12c452d53a391a3ad33587bee7431984
kermani <97584991+hkermanin@users.noreply.github.com>
2025-10-18 21:24:20 +0000

    Add owner to Factory.sol

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.27;
 
 contract Factory{
     uint256 public immutable fee;
+    address public owner;
 
     constructor(uint256 _fee){
         fee = _fee;
+        owner = msg.sender;
     }
 }
